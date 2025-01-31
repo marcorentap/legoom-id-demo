84c49e9a77c588337f4e1fea857f0d5a9daac8ca
Marc Owen <marcowen20@gmail.com>
2025-01-31 13:20:05 +0900

    Add social_url column

--- database/migrations/2025_01_29_045019_create_profiles_table.php
+++ database/migrations/2025_01_29_045019_create_profiles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('display_name');
             $table->string('avatar')->nullable()->default(null);
+            $table->string('social_url')->nullable()->default(null);
             $table->enum('membership', ['Explorer', 'Insider', 'Clique'])->default('Explorer');
             $table->timestamps();
         });
