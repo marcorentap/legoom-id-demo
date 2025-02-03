ae7f6901449ca5e5193d45d3e09459bd3d991243
Marc Owen <marcowen20@gmail.com>
2025-02-03 14:48:39 +0900

    Add profile in api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/profile', function (Request $request) {
+    return $request->user()->profile;
+})->middleware('auth:api');
