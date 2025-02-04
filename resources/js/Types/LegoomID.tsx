bdd95ae3d8a91079fcf8b8d8b75d284a80da03d9
Marc Owen <marcowen20@gmail.com>
2025-02-04 12:56:48 +0900

    Add Legoom ID types

---
+++ resources/js/Types/LegoomID.tsx
@@ -0,0 +1,12 @@
+export interface UserProfile {
+  displayName: string,
+  socialUrl: string
+  avatar: string,
+  membership: string,
+}
+
+export interface UserAccount {
+  id: string,
+  name: string,
+  email: string
+}
