d747b82b3873773bc8f24b2375e5961156c359a7
Marc Owen <marcowen20@gmail.com>
2025-02-04 15:59:49 +0900

    Center authorization form

--- resources/js/Pages/Authorize.tsx
+++ resources/js/Pages/Authorize.tsx
@@ -17,42 +17,39 @@ export default function Authorize(props: PageProps) {
   let { height, width } = useViewportSize();
 
   return (
-    <MantineProvider>
+    <>
       <Head title="Authorize" >
         <link rel="icon" href={LegoomIDLogo} />
       </Head>
-      <Container h={height} w={width}>
-        <Center>
-          <Card>
-            <Container size="md">
-              <Image src={LegoomIDLogo} />
-            </Container>
-            <br />
+      <Center h={height} w={width}>
+        <Card>
+          <Container size="md">
+            <Image src={LegoomIDLogo} />
+          </Container>
+          <br />
 
-            <p>The application <strong>{client.name}</strong> will be able to: </p>
-            <br />
+          <p>The application <strong>{client.name}</strong> will be able to: </p>
+          <br />
 
-            <List>
-              {scopes.map(scope => {
-                return <List.Item> {scope.description} </List.Item>
-              })}
-            </List>
-            <br />
+          <List>
+            {scopes.map(scope => {
+              return <List.Item> {scope.description} </List.Item>
+            })}
+          </List>
+          <br />
 
-            <form method="post" action={route('passport.authorizations.approve')}>
-              <input type="hidden" name="_token" value={csrfToken} />
-              <input type="hidden" name="client_id" value={client.id} />
-              <input type="hidden" name="auth_token" value={authToken} />
-              <input type="hidden" name="state" value={request.state} />
-              <SimpleGrid cols={2}>
-                <Button fullWidth formAction={route('passport.authorizations.approve')} color="blue" type="submit">Authorize</Button>
-                <Button fullWidth formAction={route('passport.authorizations.approve')} color="red" type="submit">Cancel</Button>
-              </SimpleGrid>
-            </form>
-          </Card>
-        </Center>
-      </Container>
-
-    </MantineProvider>
+          <form method="post" action={route('passport.authorizations.approve')}>
+            <input type="hidden" name="_token" value={csrfToken} />
+            <input type="hidden" name="client_id" value={client.id} />
+            <input type="hidden" name="auth_token" value={authToken} />
+            <input type="hidden" name="state" value={request.state} />
+            <SimpleGrid cols={2}>
+              <Button fullWidth formAction={route('passport.authorizations.approve')} color="blue" type="submit">Authorize</Button>
+              <Button fullWidth formAction={route('passport.authorizations.approve')} color="red" type="submit">Cancel</Button>
+            </SimpleGrid>
+          </form>
+        </Card>
+      </Center>
+    </>
   )
 }
