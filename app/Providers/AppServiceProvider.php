ae509c8e8a5d7c07af64d9c6f2662155aee9c338
Marc Owen <marcowen20@gmail.com>
2025-02-17 04:15:12 +0900

    Add Passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        // If enabled, plain-text secret is only visible once
+        // Passport::hashClientSecrets();
     }
 }
