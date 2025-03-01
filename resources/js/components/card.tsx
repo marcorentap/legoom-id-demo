e49413f8286c5458d3c728cb3eed71a889527d9e
Marc Owen <marcowen20@gmail.com>
2025-03-02 08:35:12 +0900

    Use global props in user profile card

--- resources/js/components/card.tsx
+++ resources/js/components/card.tsx
@@ -1,33 +1,34 @@
-import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
-import { Avatar } from "./ui/avatar"
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
-import { CircleUser } from "lucide-react"
-
-export interface ProfileCardProps {
-  name: string
-  email: string
-  display_name: string
-  social_url: string
-  profile_picture: string
-  membership: string
-}
+import { usePage } from '@inertiajs/react';
+import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
+import { CircleUser } from 'lucide-react';
+import { Avatar } from './ui/avatar';
+import { Card, CardContent, CardHeader } from './ui/card';
 
 export default function ProfileCard(props: ProfileCardProps) {
-  return (
-    <Card >
-      <CardHeader>
-      </CardHeader>
-      <CardContent>
-        <div>
-          <Avatar className="h-20 w-20 m-auto">
-            <AvatarImage src={props.profile_picture} className="h-full w-full object-cover" />
-            <AvatarFallback className="h-full w-full object-cover"><CircleUser className="h-full w-full"/></AvatarFallback>
-          </Avatar>
-        </div>
-        <div className="text-center font-bold">{props.display_name}</div>
-        <div className="text-center font-bold">{props.membership}</div>
-        <div className="text-center">{props.social_url}</div>
-      </CardContent>
-    </Card>
-  )
+    const { user } = usePage().props;
+    return (
+        <Card>
+            <CardHeader></CardHeader>
+            <CardContent>
+                <div>
+                    <Avatar className="m-auto h-20 w-20">
+                        <AvatarImage
+                            src={user.profile.profile_picture}
+                            className="h-full w-full object-cover"
+                        />
+                        <AvatarFallback className="h-full w-full object-cover">
+                            <CircleUser className="h-full w-full" />
+                        </AvatarFallback>
+                    </Avatar>
+                </div>
+                <div className="text-center font-bold">
+                    {user.profile.display_name}
+                </div>
+                <div className="text-center font-bold">
+                    {user.membership.name}
+                </div>
+                <div className="text-center">{user.profile.social_url}</div>
+            </CardContent>
+        </Card>
+    );
 }
